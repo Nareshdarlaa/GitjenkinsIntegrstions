983966d2401301540178af2f76e1530940b0d755
Nareshdarlaa <dralanaresh5@gmail.com>
2025-08-25 12:57:06 +0530

    Changes commited Again

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("My Name is Naresh, I am 22 years old and I am working in XYZ Company.");
+	System.out.println("Naresh");
     }
 }
